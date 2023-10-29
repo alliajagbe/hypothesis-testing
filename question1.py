14bc47ac2de8c8501cffba875b146a49a5f74350
alliajagbe <alliajagbe001@gmail.com>
2023-10-29 23:42:44 +0530

    power curve

---
+++ question1.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+
+mus = np.arange(4.5, 5.5, 0.1)
+
+sigma = 1
+
+replications = 1000
+n = 100
+alpha = 0.05
+
+results = []
+significant_tests = []
+
+for mu in mus:
+    for i in range(replications):
+        rejected = False
+
+
+        # generating the jth sample of size 100 from the normal distribution
+        sample = np.random.normal(mu, sigma, n)
+        sample_mean = np.mean(sample)
+        sample_std = np.std(sample)
+        t_statistic = (sample_mean - mu)/(sample_std/math.sqrt(n))
+
+        # calculating the critical value
+        critical_value = np.percentile(sample, 95)
+
+        # checking if the null hypothesis is rejected
+        if t_statistic > critical_value:
+            rejected = True
+
+        results.append(rejected)
+
+    # calculating the number of significant tests
+    significant_tests.append(np.sum(results)/replications)
+
+# plotting the power curve
+plt.plot(mus, significant_tests)
+
+
+
+
+
+
+
+
