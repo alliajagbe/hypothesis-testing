763571e7b0a5346284336db8980815c8eea57619
alliajagbe <alliajagbe001@gmail.com>
2023-10-30 10:27:58 +0530

    plot of b

--- question2.py
+++ question2.py
@@ -7,5 +7,28 @@ import scipy.stats as stats
 
 def b1(n, X):
     numerator = (1/n) * np.sum((X - np.mean(X))**3)
-    denominator = ((1/n)*np.sum(X - np.mean(X))**2)**(3/2)
+    denominator = (1/n) * np.sum((X - np.mean(X))**2)**(3/2)
     return numerator/denominator
+
+def normalDistribution(n):
+    X = np.random.normal(0, 1, n)
+    return X
+
+def samplingDistribution(n, replications):
+    b1s = []
+    for i in range(replications):
+        X = normalDistribution(n)
+        b1s.append(b1(n, X))
+    return b1s
+
+def plotResults(n, replications):
+    b1s = samplingDistribution(n, replications)
+    plt.hist(b1s, bins=10)
+    plt.title(f"Distribution of b1 for n = {n}")
+    plt.xlabel("b1")
+    plt.ylabel("Frequency")
+    plt.show()
+
+n = 20
+replications = 1000
+plotResults(n, replications)
