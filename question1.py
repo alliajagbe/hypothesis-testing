9ebe78960b8939ca30e98449e2b9587a6da9c469
alliajagbe <alliajagbe001@gmail.com>
2023-10-30 15:54:51 +0530

    mixture of two normals

--- question1.py
+++ question1.py
@@ -3,50 +3,76 @@ import matplotlib.pyplot as plt
 import math
 import scipy.stats as stats
 
-def powerCurveNormal(mus, sigma, replications, n, alpha):
+def powerCurveNormal(mus, sigma, replications, n, alpha, true_mu):
     significant_tests = []
-
-    df = n - 1
-    crit_values = stats.t.interval(1 - alpha, df, loc=0, scale=1)
+    t_tab = stats.t.ppf(1 - alpha, n - 1)
 
     for mu in mus:
         rejected_count = 0
 
-        for i in range(replications):
-            # generating the jth sample of size 100 from the normal distribution
+        for _ in range(replications):
+
             sample = np.random.normal(mu, sigma, n)
-            sample_mean = np.mean(sample)
-            sample_std = np.std(sample)
-            t_statistic = (sample_mean - mu)/(sample_std/math.sqrt(n))
 
-            # checking if the null hypothesis is rejected
-            if t_statistic > crit_values[1] or t_statistic < crit_values[0]:
+            t, _ = stats.ttest_1samp(sample, true_mu)
+
+            # Checking if the null hypothesis is rejected using the t value
+            if t > t_tab:
                 rejected_count += 1
+            
 
-        # calculating the proportion of significant tests
+        # Calculating the proportion of significant tests
         significant_tests.append(rejected_count / replications)
 
-        print(f"mu = {mu}, proportion of significant tests = {rejected_count / replications} and results = {rejected_count}")
+        print(f"mu = {mu}, proportion of significant tests = {rejected_count / replications}")
 
     return significant_tests
 
 def plotResults(mus, significant_tests, sigma, n):
-    plt.plot(mus, significant_tests)
-    plt.title(f"Power Curve for Normal Distribution with sigma = {sigma} and n = {n}")
+    plt.plot(mus, significant_tests, label=f"n = {n}")
+    plt.title(f"Power Curve for Normal Distribution")
     plt.xlabel("mu")
     plt.ylabel("Power")
-    plt.show()
 
 
 
 mus = np.arange(4.5, 6.5, 0.1)
 sigma = 1
 replications = 1000
-n = 10
 alpha = 0.05
+true_mu = 5
 
-st = powerCurveNormal(mus, sigma, replications, n, alpha)
-plotResults(mus, st, sigma, n)
+sizes = [10, 20, 100]
+for i in sizes:
+    st = powerCurveNormal(mus, sigma, replications, i, alpha, true_mu)
+    plotResults(mus, st, sigma, i)
+plt.legend()
+plt.show()
+
+#%%
+def powerCurveMixtureOfNormals(mus, sigma1, sigma2, replications, n, alpha, true_mu):
+    significant_tests = []
+    t_tab = stats.t.ppf(1 - alpha, n - 1)
+
+    for mu in mus:
+        rejected_count = 0
+
+        for _ in range(replications):
+            if np.random.uniform() < 0.9:
+                sample = np.random.normal(mu, sigma1, n)
+            else:
+                sample = np.random.normal(mu, sigma2, n)
+
+            t, _ = stats.ttest_1samp(sample, true_mu)
+
+            # Checking if the null hypothesis is rejected using the t value
+            if t > t_tab:
+                rejected_count += 1
+
+        # Calculating the proportion of significant tests
+        significant_tests.append(rejected_count / replications)
+
+        print(f"mu = {mu}, proportion of significant tests = {rejected_count / replications}")
 
 
 
